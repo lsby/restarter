23a048ad8c052e2892e1c97f7ca2c470fe0cb62a
Stephen Zhou <hi@hyoban.cc>
2023-07-20 21:21:43 +0800

    fix: Route type

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,32 +1,40 @@
 import { z } from "zod"
+import { genFullOutType } from "./utils"
+
+// TODO: add more routes
 import * as addRoute from "./add"
 import * as loginRoute from "./login"
 
+export const routes = [addRoute, loginRoute] as const
+
 export type RouteItem = {
-  path: string
+  readonly path: string
   input: z.AnyZodObject
   data: z.AnyZodObject
-  output: any
+  output: ReturnType<typeof genFullOutType>
   errCode: readonly string[]
 }
 
-export const routes = [addRoute, loginRoute] as const
-
-export type Route = Record<
-  typeof addRoute.path,
+type Helper<T extends RouteItem> = Record<
+  T["path"],
   {
-    input: z.infer<typeof addRoute.input>
-    data: z.infer<typeof addRoute.data>
-    output: z.infer<typeof addRoute.output>
-    errCode: typeof addRoute.errCode
+    input: z.infer<T["input"]>
+    data: z.infer<T["data"]>
+    output: z.infer<T["output"]>
+    errCode: T["errCode"]
   }
-> &
-  Record<
-    typeof loginRoute.path,
-    {
-      input: z.infer<typeof loginRoute.input>
-      data: z.infer<typeof loginRoute.data>
-      output: z.infer<typeof loginRoute.output>
-      errCode: typeof loginRoute.errCode
-    }
-  >
+>
+
+type DistributedHelper<T> = T extends RouteItem ? Helper<T> : never
+
+type ArrayToUnion<T extends readonly any[]> = T[number]
+
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I,
+) => void
+  ? I
+  : never
+
+export type Route = UnionToIntersection<
+  DistributedHelper<ArrayToUnion<typeof routes>>
+>
